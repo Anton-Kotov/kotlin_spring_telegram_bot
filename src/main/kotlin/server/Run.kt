18fdd01e79145e52a4dd3727ede6663a8487837b
Котов Антон <a.kotov@okenit.ru>
2023-08-18 13:44:01 +0300

    запуск бота при старте

--- src/main/kotlin/server/Run.kt
+++ src/main/kotlin/server/Run.kt
@@ -4,6 +4,8 @@ import BotProperties
 import com.github.kotlintelegrambot.echo.Bot
 import com.github.kotlintelegrambot.echo.keyboards.keyboard
 import com.github.kotlintelegrambot.entities.ChatId
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
@@ -12,16 +14,19 @@ import org.springframework.web.bind.annotation.*
 
 data class PostData(val message: String, val id: String, val buttons: List<Map<String, String>>)
 
-
 @SpringBootApplication(scanBasePackages = ["package com.github.kotlintelegrambot.echo", "server"])
 @RestController
 @EnableConfigurationProperties(BotProperties::class)
 class Application (private val botProperties: BotProperties,
-				   private val botTelegram: Bot) {
+				   private val botTelegram: Bot) : ApplicationRunner {
+
+	override fun run(args: ApplicationArguments?) {
+		// Запуск бота при старте приложения
+		botTelegram.bot.startPolling()
+	}
 	@PostMapping("/sendMessage")
 	fun sendMessage(@RequestBody postData: PostData): ResponseEntity<String> {
-		// Запускаем бота при получении запроса
-		botTelegram.bot.startPolling()
+
 		val currentChatId = botProperties.currentChatId
 		val keyboardMarkup = keyboard(postData)
 
